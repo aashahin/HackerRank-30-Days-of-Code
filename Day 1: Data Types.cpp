5267bca1ce3ff019b2e3ce6ae50679c9d653eca6
Abdelrahman Shaheen <39490183+aashahin@users.noreply.github.com>
2023-02-19 09:28:09 +0000

    Day 1

---
+++ Day 1: Data Types.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    
+// Declare second integer, double, and String variables.
+
+    int a;
+    double b;
+    string c;
+// Read and save an integer, double, and String to your variables.
+
+    cin >> a;
+    cin >> b;
+    getline(cin, c);
+    getline(cin, c);
+
+// Print the sum of both integer variables on a new line.
+    cout << (a + i) << endl;
+// Print the sum of the double variables on a new line.
+    cout << setprecision(1) << fixed << (b + d) << endl;
+// Concatenate and print the String variables on a new line
+    cout << (s + c) << endl;
+// The 's' variable above should be printed first.
+    return 0;
+}
